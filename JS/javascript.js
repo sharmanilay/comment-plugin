a4e75217d8eccdb6602dcdf9493c3b8f4eed1e82
Nilay sharma <nilaysharma642@gmail.com>
2018-09-05 00:16:58 +0530

    Up/down vote implemented

--- JS/javascript.js
+++ JS/javascript.js
@@ -74,34 +74,76 @@ class Comment {
         const Name = document.createElement('span');
         const span = document.createElement('span')
         const del = document.createElement('a')
-        const votes = document.createElement('span');
+        const rep = document.createElement('a')
         const time  = document.createElement('span');
+        const vcont = document.createElement('span');
+        const votes = document.createElement('span');
+        const up = document.createElement('button');
+        const down = document.createElement('button');
 
         //setting values
+        vcont.classList.add('vcount');
         Name.innerText = currentUser
+        Name.classList.add('title','username')
+
         const Ct = data.Comments[id]
         votes.classList.add('votes')
         votes.innerText = Ct.votes
-        Name.classList.add('title','username')
+
         span.classList.add('comment')
         span.innerText = comment
+
         time.innerText = hours+":"+minutes
         time.classList.add('time')
+
+        up.innerHTML = '&#x21e7';
+        up.classList.add('btn-flat')
+        up.addEventListener("click", function() {
+          Ct.votes++;
+          votes.innerText = Ct.votes
+          up.classList.add('disabled');
+          down.classList.remove('disabled');
+        })
+        up.classList.add('vbutton')
+
+        down.innerHTML = '&#x21e9';
+        down.classList.add('btn-flat')
+        down.addEventListener("click", function() {
+          Ct.votes--;
+          votes.innerText = Ct.votes
+          down.classList.add('disabled');
+          up.classList.remove('disabled');
+        })
+        down.classList.add('vbutton')
+
         del.innerText = 'delete'
         del.setAttribute('data-delete-id',id)
+
+        rep.innerText = "reply"
+        rep.setAttribute('data-rep-id', id)
+        rep.addEventListener("click", e => {
+          e.preventDefault();
+          console.log("I will write a reply");
+        })
+
         li.classList.add('collection-item');
 
 
         //Putting elements in the dom
         this.ul.appendChild(li)
         li.appendChild(Name)
+        li.appendChild(time)
         li.appendChild(document.createElement('br'))
         li.appendChild(del)
-        li.appendChild(time)
+        li.appendChild(rep)
         li.appendChild(span)
-        li.appendChild(votes)
+        li.appendChild(vcont)
+        vcont.appendChild(up)
+        vcont.appendChild(votes)
+        vcont.appendChild(down)
         this.items[id] = li
       }
+
       removeComment(id) {
         // state logic
         const li = this.items[id]
